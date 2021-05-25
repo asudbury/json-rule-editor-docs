b4bc6a7b5826e993b14ed75470a7d1fa10332fc6
Adrian Sudbury <sudburya@gmail.com>
2021-05-25 09:24:02 +0100

    Remove source code

--- src/constants/data-types.js
+++
@@ -1,3 +0,0 @@
-export const DATA_TYPES = {STRING: 'string', BOOLEAN: 'boolean', NUMBER: 'number', ARRAY: 'array'};
-
-export const PLACEHOLDER = { 'string': 'Enter value', 'number': 'Enter numerical value', 'array': 'Ex: item1,item2,item3'};
--- src/utils/stringutils.js
+++
@@ -1,7 +0,0 @@
-
-
-export const isContains = (str1, str2) => {
-    const lowercasestr1 = str1.toLowerCase();
-    const lowercasestr2 = str2.toLowerCase();
-    return lowercasestr1.indexOf(lowercasestr2) > -1;
-}
